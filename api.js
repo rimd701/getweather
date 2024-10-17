9a33ad4b3ec52f0e07642b8abbde4d2bdfa69f3e
rimd701 <rimd701@gmail.com>
2024-10-17 04:23:21 +0200

    change1

--- api.js
+++ api.js
@@ -1,20 +1,24 @@
 exports.handler = async (event, context) => {
-    if (event.httpMethod === 'GET') {
-      try {
-        // Process the GET request as needed
-        const data = require('./db.json');
-  
-        // Return the data as the response
-        return {
-          statusCode: 200,
-          body: JSON.stringify(data),
-        };
-      } catch (error) {
-        // Return an error response if there was an issue processing the request
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ error: 'Failed to process GET request' }),
-        };
-      }
+  if (event.httpMethod === "GET") {
+    const headers = {
+      "Access-Control-Allow-Origin": "*", // Replace * with the appropriate domain
+      "Access-Control-Allow-Headers": "Content-Type",
+    };
+    try {
+      // Process the GET request as needed
+      const data = require("./db.json");
+
+      // Return the data as the response
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+    } catch (error) {
+      // Return an error response if there was an issue processing the request
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: "Failed to process GET request" }),
+      };
     }
-  };
+  }
+};
